053ae3c27605af16cfafb78ba516fea2b8585309
Richard Schneiderman <schneiderman.richard@yahoo.com>
2017-03-20 11:25:07 -0400

    added mobox

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {observer} from 'mobx-react';
+import {observable, computed} from 'mobx'
 
-class HelloWorld extends React.Component {
-    render() {
-        return <h1>Helsso s5fro6eserwerwe {this.props.phrase}!</h1>;
-    }
+class Question {
+  choices = [{label : "nbc", value : 1},{label : "cbs", value : 2}]
+  name = "channel"
+  title = "Richard"
+
 }
 
+class QuestionView extends Component {
 
-ReactDOM.render(
-    <HelloWorld phrase="ES6"/>,
-    document.getElementById('app')
-);
+  
+   render() {
+        const cbs = this.props.question.choices.map((c) =>
+          <div>
+            <input type="radio" name="options" value={c.value}/>{c.label}
+          </div>
+        );
+       return(
+          <div>
+          {cbs}
+          </div>
+       )
+
+  }
+} ;
+
+var question = new Question() 
+
+ReactDOM.render(<QuestionView question={question}/>, document.getElementById('app'));
